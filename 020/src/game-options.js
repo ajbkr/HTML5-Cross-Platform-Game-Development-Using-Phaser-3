56d7458792ce16db7d661708da296e4b3ff39cc4
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-04-05 22:29:39 +0100

    Add sound effects

---
+++ 020/src/game-options.js
@@ -0,0 +1,24 @@
+export default {
+  boardSize: {
+    cols: 4,
+    rows: 4
+  },
+  swipeMaxTime: 1000,
+  swipeMinDistance: 20,
+  swipeMinNormal: 0.85,
+  tileSize: 200,
+  tileSpacing: 20,
+  tweenSpeed: 50
+}
+
+const LEFT = 0
+const RIGHT = 1
+const UP = 2
+const DOWN = 3
+
+export const directions = {
+  LEFT,
+  RIGHT,
+  UP,
+  DOWN
+}
